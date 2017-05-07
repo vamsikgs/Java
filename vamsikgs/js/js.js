620b0f9af5990dfa8c2b6412615ed975a2bd078e
vamsikgs <vamsikgs@gmail.com>
2017-05-07 16:27:33 -0500

    Add files via upload

---
+++ vamsikgs/js/js.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('h1').textillate({ 
+        in: { 
+        effect: 'bounceInDown',
+        shuffle: true,
+        }
+    });
+
+    $('.town').textillate({
+        loop: true,
+        initialDelay: 1000,
+        minDisplayTime: 1000,
+        in:{
+        effect: 'wobble',
+        sync: true,
+        },
+        
+        out:{
+        effect: 'wobble',
+        sync: true,
+        }
+    });
+    
+    $('.message').click(function() {
+        $(this).text(function(i, text){
+          return text === 'send me a message' ? 'send me a message to vamsikgs@gmail.com' : 'send me a message';
+        });
+        $(this).toggleClass('message');
+    });
+});
